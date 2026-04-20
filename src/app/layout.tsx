8e5dd08b2173bce28f8dd6b73830962814d2b76a
nhatduysecond <nhatduysecond@gmail.com>
2026-04-20 17:47:01 +0700

    Update tilte of website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'Nexora Technology',
+  title: 'Nexora Technology Co., Ltd. | Software Technology Company.',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
